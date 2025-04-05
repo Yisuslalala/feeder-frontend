6a32d8f380b077274f2ddc01baaca8ad1a8000a3
Yisuslalala <wardodev1.00@gmail.com>
2025-04-04 23:57:20 -0600

    Structure folders and base component finished

---
+++ src/components/FeederApp.jsx
@@ -0,0 +1,15 @@
+export const FeederApp = () => {
+
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center container">
+        <h1 className="header-title fw-800">Feeder App</h1>
+      </div>
+      <div className="d-flex justify-content-center feeder">
+        <div>
+          {/* here goes feeders component */}
+        </div>
+      </div>
+    </>
+  );
+}
