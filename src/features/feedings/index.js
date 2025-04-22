45031b760f73f56a90e849ede65d05b705ee002a
Yisuslalala <wardodev1.00@gmail.com>
2025-04-21 22:37:30 -0600

    Alias implemented for feature-base structure

---
+++ src/features/feedings/index.js
@@ -0,0 +1,4 @@
+export { default as Feedings } from "./components/Feedings.jsx";
+export { default as Feeding } from "./components/Feeding.jsx";
+
+export { fetchFeedings } from "./services/fetchFeedings.jsx";
