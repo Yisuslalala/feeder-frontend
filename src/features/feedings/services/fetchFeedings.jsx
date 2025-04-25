5b490daf85df93cd168fcb0e356d4eefa64990c9
Yisuslalala <wardodev1.00@gmail.com>
2025-04-24 19:07:19 -0600

    Fetch cleaned for better code maintenence

--- src/features/feedings/services/fetchFeedings.jsx
+++ src/features/feedings/services/fetchFeedings.jsx
@@ -8,20 +8,5 @@ export const fetchFeedings = async () => {
   const response = await fetch(`http://${apiUrl}/details`);
   const data = await response.json();
   return await data;
-}
-
-export const createFeeding = async () => {
-  const res = fetch(apiUrl, {
-    method: `POST`,
-    headers: { 'Content-type': 'application/json' },
-    // body: JSON.strigify(),
-  });
-
-  if (!res.ok) {
-    throw new Error(`Error at POST request`);
-  }
-
-  return await res.json();
 };
 
-// TODO: Finish CRUD functions
